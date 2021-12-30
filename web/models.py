8e92092ce86b6281dddea6d89afdc3379163371c
Mohammad Bagher Hesam <m.b.hesam@gmail.com>
2021-12-30 09:16:42 +0330

    expense model created

--- web/models.py
+++ web/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import User
+class Expense(models.Model):
+    text = models.CharField(max_length=255)
+    amount = models.BigIntegerField()
+    date = models.DateTimeField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
